35d044f17def93e1d17d6380563262fefe21d112
artemkms <69520287+artemkms@users.noreply.github.com>
2022-10-19 21:04:48 +0700

    workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -76,6 +76,28 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+#PG
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv(
+#             'DB_ENGINE',
+#             default='django.db.backends.postgresql'),
+#         'NAME': os.getenv(
+#             'POSTGRES_DB',
+#             default='postgres'),
+#         'USER': os.getenv(
+#             'POSTGRES_USER',
+#             default='postgres'),
+#         'PASSWORD': os.getenv(
+#             'POSTGRES_PASSWORD',
+#             default='postgres'),
+#         'HOST': os.getenv(
+#             'DB_HOST',
+#             default='db'),
+#         'PORT': os.getenv(
+#             'DB_PORT',
+#             default='5432'),
+#     }}
 
 
 # Password validation
