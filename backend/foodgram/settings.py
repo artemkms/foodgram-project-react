a2d7a487712175d0d8374b9bd5798d5a508f3cda
artemkms <69520287+artemkms@users.noreply.github.com>
2022-10-22 08:57:03 +0700

    ingr

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-#  }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+ }
 #PG
 
 DATABASES = {
